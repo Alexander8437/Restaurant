ebc8a85b6375cfd3dcf8d3bef180b17efc775a60
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2024-03-07 11:32:53 +0530

    8 commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,18 +5,18 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left">ALEXANDER DUNG DUNG</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
-            <p>Open: 05:00 PM - 12:00 AM</p>
+            <p>COURT ROAD, SAHARANPUR.</p>
+            <p>Open: 05:00 PM - 01:00 AM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By ALEXANDER</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By MysSelf.</p>
           </div>
         </div>
       </div>
