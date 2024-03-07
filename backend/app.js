0c3951aa218e064e2ff105cba1d191abec4817a8
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2024-03-07 13:24:15 +0530

    12 commit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import bodyParser from "body-parser";
 import { errorMiddleware } from "./middlewares/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
 import { dbConnection } from "./database/dbConnection.js";
@@ -9,8 +10,8 @@ const app = express();
 dotenv.config({ path: "./config/config.env" });
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/v1/reservation", reservationRouter);
 app.get("/", (req, res, next) => {
